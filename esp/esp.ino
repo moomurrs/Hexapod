eb1af2e97cca3088a1d7ff7dad2f1bbbb95fe0d8
MT <59849596+moomurrs@users.noreply.github.com>
2023-11-03 16:19:54 -0400

    send 4 ints, each broken into 2 bytes

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,24 +1,64 @@
-
 #include <Wire.h>
 #include <stdio.h>
 
 #define I2CADDR 0x55
 
-unsigned char value = 0;
+// int is 2 bytes
+unsigned int rx = 0;
+unsigned int ry = 0;
+unsigned int lx = 0;
+unsigned int ly = 0;
 
 void setup() {
   Wire.setClock(100000);
   Wire.begin(I2CADDR);
   Wire.onRequest(requestEvent);
-  Wire.write(value);
 
   Serial.begin(115200);
 }
 
 void loop() {
-  delay(100);
+
+  //delay(500);
 }
 
 void requestEvent() {
-  Wire.write(value++);
+
+  const unsigned char mask = 0xFF;
+
+  // send rx
+  unsigned char higher_char = (rx >> 8) & mask;
+  unsigned char lower_char = rx & mask;
+
+  // write can only send 1 byte at a time
+  Wire.write(higher_char);
+  Wire.write(lower_char);
+
+  // send ry
+  higher_char = (ry >> 8) & mask;
+  lower_char = ry & mask;
+
+  Wire.write(higher_char);
+  Wire.write(lower_char);
+
+  // send lx
+  higher_char = (lx >> 8) & mask;
+  lower_char = lx & mask;
+
+  Wire.write(higher_char);
+  Wire.write(lower_char);
+
+  // send ly
+  higher_char = (ly >> 8) & mask;
+  lower_char = ly & mask;
+
+  Wire.write(higher_char);
+  Wire.write(lower_char);
+
+
+  rx++;
+  ry += 2;
+  lx += 4;
+  ly += 8;
+
 }
